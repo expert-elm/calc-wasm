f969de31e02b54034b0136b6cf544823d0da331f
LinGo <saixmh@163.com>
2019-01-11 15:25:57 +0800

    test with sin, cos

--- tests/foo.rs
+++ tests/foo.rs
@@ -1,36 +1,48 @@
 #[test]
 fn add() {
-    let result = finger::calc("1 + 1".to_owned());
+    let result = finger::calc("1 + 1".into());
     assert_eq!(result, "2");
 }
 
 #[test]
 fn sub() {
-    let result = finger::calc("234 - 1".to_owned());
+    let result = finger::calc("234 - 1".into());
     assert_eq!(result, "233");
 }
 
 #[test]
 fn mul() {
-    let result = finger::calc("128 * 2".to_owned());
+    let result = finger::calc("128 * 2".into());
     assert_eq!(result, "256");
 }
 
 #[test]
 fn div() {
-    let result = finger::calc("1024 / 2".to_owned());
+    let result = finger::calc("1024 / 2".into());
     assert_eq!(result, "512");
 }
 
 #[test]
+fn sin() {
+    let result = finger::calc("sin(90)".into());
+    assert_eq!(result, "1");
+}
+
+#[test]
+fn cos() {
+    let result = finger::calc("cos(0)".into());
+    assert_eq!(result, "1");
+}
+
+#[test]
 fn add_mul() {
-    let result = finger::calc("1 + 1 * 2".to_owned());
+    let result = finger::calc("1 + 1 * 2".into());
     assert_eq!(result, "3");
 }
 
 #[test]
 fn add_div() {
-    let result = finger::calc("2 + 4 / 2".to_owned());
+    let result = finger::calc("2 + 4 / 2".into());
     assert_eq!(result, "4");
 }
 
